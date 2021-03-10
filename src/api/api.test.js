0803c71754d6be0c7b9c297d184b57f27c379ff7
will <will.eizlini@gmail.com>
2021-03-10 16:55:19 -0500

    added functionality for user repos and in test

--- src/api/api.test.js
+++ src/api/api.test.js
@@ -1,7 +1,12 @@
 import fetch from 'isomorphic-fetch'
 import getRepos from "./getRepos";
 
-test("test github api", async ()=>{
+test("test getRepos with an org", async ()=>{
   const repos = await getRepos("iptoki");
   expect(repos[0].id).toBe(186482231)
+})
+
+test("test getRepos with a user", async ()=>{
+  const repos = await getRepos("weizlini");
+  expect(repos[0].id).toBe(202314895)
 })
--- src/api/getRepos.js
+++ src/api/getRepos.js
@@ -1,6 +1,17 @@
 import fetch from 'isomorphic-fetch'
 
-export default async function getRepos(org){
-   const response = await fetch(`https://api.github.com/orgs/${org}/repos`)
-   return response.json()
+export default async function getRepos(orgOrUser){
+   try {
+      const response = await fetch(`https://api.github.com/orgs/${orgOrUser}/repos`)
+      if (response.ok)
+         return response.json()
+      else {
+         const userResponse = await fetch(`https://api.github.com/users/${orgOrUser}/repos`)
+         if(userResponse.ok)
+            return userResponse.json()
+      }
+
+   }catch(e){
+      console.error('fetch error: ', e);
+   }
 }
